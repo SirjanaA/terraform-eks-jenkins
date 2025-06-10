528f709d701f5d3805c6526f690da60436ca98fa
SirjanaA <alesrizu92@icloud.com>
2025-06-11 00:19:04 +0100

    removed intra_subnet

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -23,7 +23,6 @@ locals {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
-  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
   eks_node_group_subnets = ["10.0.7.0/24", "10.0.8.0/24"] # Added for EKS node groups
   tags = {
     Example = local.name
